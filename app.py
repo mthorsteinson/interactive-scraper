7d38f28a9ded2997f82d7433c052ea8ed30910c8
mthorsteinson <mjthorsteinson@gmail.com>
2025-05-06 10:27:55 -0600

    renamed CLI program and fixed webapp errors

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = flask.Flask(__name__)
 colorama.init(autoreset=True)
 api_key = "9558ec233d504b21baccdf101b12ceed"
 url = "https://newsapi.org/v2/top-headlines/"
+choices = ["business", "entertainment", "general", "health", "science", "sports", "technology"]
 
 
 def get_headlines(country="us",category="technology"):
@@ -24,12 +25,16 @@ def get_headlines(country="us",category="technology"):
 
     if response.status_code != 200 or data.get("status") != "ok":
         print(colorama.Fore.RED + f"That didn't work\n{data.get('message')}")
-        return
-
+        return [], f"Error: {data.get('message', 'Unknown error')}"
+    '''
     print(f"\nTop 5 {category.title()} Headlines in {country.upper()}:\n")
     for i, article in enumerate(data["articles"], 1):
         print(f"{i}. {article['title']}")
         print(f" -- {article['url']}\n")
+    '''
+    articles = data["articles"][:5]  # Get top 5 articles
+    return articles, None
+
 
 def pick_country():
     fakecountry = input("Please enter a country code: \nApologies, the only available country right now is the US (any key to continue): ").lower().strip()
@@ -39,7 +44,6 @@ def pick_country():
 def pick_category():
     category = input("Please enter a news category:\nOptions -- " + colorama.Fore.GREEN + "Business, Entertainment, General, Health, Science, Sports, Technology\n" + colorama.Style.RESET_ALL).lower().strip()
     global choices
-    choices = ["business", "entertainment", "general", "health", "science", "sports", "technology"]
     if category not in choices:
         print(colorama.Fore.RED + f"\nThat's not a valid category. Try again.\n")
         category = pick_category()
