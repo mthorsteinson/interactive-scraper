d193c55f6ca5b64ef85f86f3ca5c2d568ad20587
mthorsteinson <mjthorsteinson@gmail.com>
2025-05-16 08:45:37 -0600

    remove search notes

--- app.py
+++ app.py
@@ -99,21 +99,6 @@ def index3():
     else:
         return flask.render_template("failure.html",articles=articles,error=error,listitems=listitems,category=category)
     
-''' ## Implement a search route with new functionalities
-## Need to include a template for searches as well
-@app.route("/", methods=["GET","POST"])
-def search():
-    articles = []
-    error = None
-    listitems = choices1
-    category = flask.request.form.get("category", "technology").capitalize()
-    if flask.request.method in ["POST","GET"]:
-        articles, error = get_headlines("us",category)
-    if error == None:
-        return flask.render_template("search.html",articles=articles,error=error,listitems=listitems,category=category)
-    else:
-        return flask.render_template("failure.html",articles=articles,error=error,listitems=listitems,category=category)
-'''
             
 @app.route("/failure", methods=["GET","POST"])
 def failure():
